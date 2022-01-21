52655327fd688c437c8495fae8d494cc589dbf40
catalin.marginean1 <catalin.marginean1@gmail.com>
2022-01-21 23:08:55 +0200

    small correction 2

--- Migrations/20220121210702_Materie.Designer.cs
+++ Migrations/20220121210702_Materie.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Orar.Data;
 
 namespace Orar.Migrations
 {
     [DbContext(typeof(OrarContext))]
-    partial class OrarContextModelSnapshot : ModelSnapshot
+    [Migration("20220121210702_Materie")]
+    partial class Materie
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20220121210702_Materie.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Orar.Migrations
+{
+    public partial class Materie : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Clasa",
+                table: "OrarCls");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrarCls_ClasaID",
+                table: "OrarCls",
+                column: "ClasaID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrarCls_Clasa_ClasaID",
+                table: "OrarCls",
+                column: "ClasaID",
+                principalTable: "Clasa",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrarCls_Clasa_ClasaID",
+                table: "OrarCls");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrarCls_ClasaID",
+                table: "OrarCls");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Clasa",
+                table: "OrarCls",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
